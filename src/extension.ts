7593a05425e403971a9532955a623cab35f2ad27
Liu Wei <419555308@qq.com>
2017-08-28 22:43:18 +0800

    aa

---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { exec, ChildProcess } from 'child_process';
+var child: ChildProcess;
+function spownChild() {
+    let c = exec(`tsc -w -p ${vscode.workspace.rootPath}`);
+    c.stdout.on('data', data => { });
+    c.stderr.on('data', data => console.error(data));
+    c.on('exit', (e) => {
+        child = spownChild();
+    });
+    return c;
+}
+export function activate(context: vscode.ExtensionContext) {
+    child = spownChild();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    if (child) {
+        child.kill();
+    }
+}
